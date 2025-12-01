b344cca32d484825bffb25d05a764371467e0a94
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2025-12-01 12:02:27 +0900

    [update]カメラが向いている方向に移動できるようにした

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,22 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     private Rigidbody _rb;
+    Camera _camera;
     private Vector3 _moveVector;
     void Start()
     {
+        _camera = GetComponent<Camera>();
         _rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
+        _camera = Camera.main;
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+
         _moveVector = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        _rb.linearVelocity = _moveVector.normalized * speed;
+        _moveVector = _camera.transform.TransformDirection(_moveVector);
+        _rb.linearVelocity = _moveVector * speed;
+        transform.rotation = new Quaternion(0, _camera.transform.rotation.y, 0, _camera.transform.rotation.w);
     }
 }
